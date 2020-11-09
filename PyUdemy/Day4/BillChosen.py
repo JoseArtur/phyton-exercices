987abd105b0e1fc25f68ba03f15a86912a4089d6
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-11-09 16:34:03 +0000

    New exercises in day 4

---
+++ PyUdemy/Day4/BillChosen.py
@@ -0,0 +1,14 @@
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+names = namesAsCSV.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+lenNames = (len(names)-1)
+rnumber=random.randint(0,int(lenNames))
+print(f"{names[rnumber]} is going to pay the bill today!!")
