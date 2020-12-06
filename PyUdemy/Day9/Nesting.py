164ffa9f1b87cecccc9b02aa7ab6a5d7d46e9a1d
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-12-06 15:37:01 +0000

    Update Nesting.py

--- PyUdemy/Day9/Nesting.py
+++ PyUdemy/Day9/Nesting.py
@@ -1,6 +1,24 @@
-#A nested diccionary, how you can see, a dictionary inside another
+# A nested diccionary, how you can see, a dictionary inside another
 travel_log = {
-    "France:": {"cities_france" : ["Paris", "Lille", "Dijon"], "total_visitors":124,"languages_spoken":["Francais,Anglais,Allemande"]},
-    "Germany:":{"cities_germany": ["Berlin", "Hamburg", "Stuttgart"], "languages_spoken":["Deutsch","Englisch"]},
+    "France:": {
+        "cities_france": ["Paris", "Lille", "Dijon"],
+        "total_visitors": 124,
+        "languages_spoken": ["Francais,Anglais,Allemande"]
+        },
+    "Germany:": {
+        "cities_germany": ["Berlin", "Hamburg", "Stuttgart"], 
+        "languages_spoken": ["Deutsch", "Englisch"]
+        },
 }
-print(travel_log)
+# A list of diccionaries
+travel_loglist = [
+    {"country": "France:",
+     "cities_france": ["Paris", "Lille", "Dijon"],
+     "total_visitors":124, 
+     "languages_spoken":["Francais,Anglais,Allemande"],
+     },
+    {"country": "Germany:", 
+    "cities_germany": ["Berlin", "Hamburg", "Stuttgart"], 
+    "languages_spoken":["Deutsch", "Englisch"],
+    },
+]
