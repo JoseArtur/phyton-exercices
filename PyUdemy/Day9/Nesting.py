c13bd8789b1df6e9c691989f73c2ca48c4f046dd
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-12-06 15:28:39 +0000

    Create Nesting.py

---
+++ PyUdemy/Day9/Nesting.py
@@ -0,0 +1,6 @@
+#A nested diccionary, how you can see, a dictionary inside another
+travel_log = {
+    "France:": {"cities_france" : ["Paris", "Lille", "Dijon"], "total_visitors":124,"languages_spoken":["Francais,Anglais,Allemande"]},
+    "Germany:":{"cities_germany": ["Berlin", "Hamburg", "Stuttgart"], "languages_spoken":["Deutsch","Englisch"]},
+}
+print(travel_log)
