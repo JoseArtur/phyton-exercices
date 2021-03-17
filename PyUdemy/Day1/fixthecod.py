e3b2bf5ddd0056bfcb0cb10523b9b37b19c8a120
José Artur <65734671+JoseArtur@users.noreply.github.com>
2021-03-17 16:23:07 +0000

    .

--- PyUdemy/Day1/fixthecod.py
+++ PyUdemy/Day1/fixthecod.py
@@ -3,7 +3,7 @@
 print(Day 1 - String Manipulation")
 print("String Concatenation is done with the "+" sign.")
   print('e.g. print("Hello " + "world")')
-print(("New lines can be created with a backslash and n.") """
+print(("New lines can be created with a backslasjngsljndlfh and n.") """
 #Fix the code below 👇
 
 print("Day 1 - String Manipulation")
